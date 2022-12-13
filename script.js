bb37d6e41139defb0e8f1daea72986ae7184cd24
DESKTOP-1QJ3QEI <syednadeemali221@gmail.com>
2022-12-13 15:44:14 +0500

    complete

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500){
+            $('.scroll-up-button').addClass('show');
+        }else{
+            $('.scroll-up-button').removeClass('show')
+        }
+    });
+
+    // slide up Script
+    $('.scroll-up-button').click(function(){
+        $('html').animate({scrollTop: 0});
     });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
+    });
+
+    // typeing animation effect
+
+    var typed = new Typed(".typing",{
+        strings: [ "Youtuber", "Developer ", "Blogger", "Designer", "Freelancer" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2",{
+        strings: [ "Youtuber", "Developer ", "Blogger", "Designer", "Freelancer" ],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
     });
 
     // owl carousel script
